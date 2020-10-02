67caaf95c5813251931d73d74528114350c2c4eb
Andrey Slotin <andrey.slotin@instana.com>
2020-10-02 12:31:52 +0200

    Log commit sha and repo

--- main.go
+++ main.go
@@ -35,9 +35,8 @@ func main() {
 	)
 	tc := oauth2.NewClient(ctx, ts)
 
-	log.Printf("GITHUB_REF=%s", os.Getenv("GITHUB_REF"))
-	log.Printf("GITHUB_BASE_REF=%s", os.Getenv("GITHUB_BASE_REF"))
-	log.Printf("GITHUB_HEAD_REF=%s", os.Getenv("GITHUB_HEAD_REF"))
+	log.Printf("GITHUB_REF=%s", os.Getenv("GITHUB_REPOSITORY"))
+	log.Printf("GITHUB_REF=%s", os.Getenv("GITHUB_SHA"))
 
 	owner, repo, err := splitRepositoryName(os.Getenv("GITHUB_REPOSITORY"))
 	if err != nil {
